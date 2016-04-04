f6dc9c9ce4e56823ecefe2d2471101e27ba107d5
Alexander Zuev <zuev08@gmail.com>
2016-04-04 21:26:37 +0300

    Change cmd args

--- AD/src/auto_BSA/Main.java
+++ AD/src/auto_BSA/Main.java
@@ -14,67 +14,67 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 
 public class Main {
-	static String  NameOfFunc = null;
+	static String NameOfFunc = null;
 	public static void main(String[] args) throws FileNotFoundException {
 		if (args.length == 0) {
 			System.out.println("Command line arguments:");
-			System.out.print("nameOfFileToRead nameOfFunction ");
-			System.out.println("[json | fromjson filename]");
+			System.out.println("fromjson filename | nameOfFileToRead nameOfFunction [json]");
 			System.out.println("if nameOfFunction is missing, reading 1st function in the file");
 		}
 		if (args.length > 0) {
-			FileReader file = new FileReader(args[0]);
-			Scanner scanner = new Scanner(file);
-			StringBuilder code = new StringBuilder();
-			while (scanner.hasNextLine()) {
-				code.append(scanner.nextLine() + '\n');
-			}
-			Interpreter icpp = new Interpreter(code.toString());
-			icpp.deleteComments();
-			Block[] b;
+			Block[] b = null;
 			if (args.length > 1) {
-				NameOfFunc = args[1];
-				b = icpp.analyze(args[1]).toArray(new Block[0]);
-				if (args.length > 2) {
-					if (args[2].equals("json")) {
-						try (BufferedWriter writer = new BufferedWriter(new FileWriter(NameOfFunc + ".json"))) {
-							String json = Interpreter.toJson(b[0]);
-							writer.write(json);
-						} catch (JsonProcessingException e) {
-							System.err.println("An error has been occured:");
-							e.printStackTrace();
-						} catch (IOException e) {
-							System.err.println("An I/O error has been occured:");
-							e.printStackTrace();
-						}
-					} else if (args[2].equals("fromjson") && args.length > 3) {
-						String jsonFilename = args[3];
-						try (BufferedReader reader = new BufferedReader(new FileReader(jsonFilename))) {
-							StringBuilder json = new StringBuilder();
-							String line;
-							do {
-								line = reader.readLine();
-								if (line != null) {
-									json.append(line);
-								}
-							} while (line != null);
-							b = Interpreter.fromJson(json.toString()).toArray(new Block[0]);
-						} catch (JsonParseException e) {
-							System.err.println("Parsing JSON error:");
-							e.printStackTrace();
-						} catch (JsonMappingException e) {
-							System.err.println("An error has been occured:");
-							e.printStackTrace();
-						} catch (IOException e) {
-							System.err.println("An I/O error has been occured:");
-							e.printStackTrace();
+				if (args[0].equals("fromjson")) {
+					String jsonFilename = args[1];
+					try (BufferedReader reader = new BufferedReader(new FileReader(jsonFilename))) {
+						StringBuilder json = new StringBuilder();
+						String line;
+						do {
+							line = reader.readLine();
+							if (line != null) {
+								json.append(line);
+							}
+						} while (line != null);
+						b = Interpreter.fromJson(json.toString()).toArray(new Block[0]);
+						NameOfFunc = args[1].replaceAll(".json$", "");
+					} catch (JsonParseException e) {
+						System.err.println("Parsing JSON error:");
+						e.printStackTrace();
+					} catch (JsonMappingException e) {
+						System.err.println("An error has been occured:");
+						e.printStackTrace();
+					} catch (IOException e) {
+						System.err.println("An I/O error has been occured:");
+						e.printStackTrace();
+					}
+				} else {
+					FileReader file = new FileReader(args[0]);
+					Scanner scanner = new Scanner(file);
+					StringBuilder code = new StringBuilder();
+					while (scanner.hasNextLine()) {
+						code.append(scanner.nextLine() + '\n');
+					}
+					scanner.close();
+					Interpreter icpp = new Interpreter(code.toString());
+					icpp.deleteComments();
+					NameOfFunc = args[1];
+					b = icpp.analyze(args[1]).toArray(new Block[0]);
+					if (args.length > 2) {
+						if (args[2].equals("json")) {
+							try (BufferedWriter writer = new BufferedWriter(new FileWriter(NameOfFunc + ".json"))) {
+								String json = Interpreter.toJson(b[0]);
+								writer.write(json);
+							} catch (JsonProcessingException e) {
+								System.err.println("An error has been occured:");
+								e.printStackTrace();
+							} catch (IOException e) {
+								System.err.println("An I/O error has been occured:");
+								e.printStackTrace();
+							}
 						}
 					}
 				}
-			} else {
-				b = icpp.analyze().toArray(new Block[0]);				
 			}
-			scanner.close();
 			new DrawerSimple(b[0]).setVisible(true);
 		}
 	}
